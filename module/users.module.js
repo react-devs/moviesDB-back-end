f3fac8302ae8c0c12a18013b6076e2dc15d706de
SufianHamdan <sufian_hamdan@yahoo.com>
2021-05-24 00:43:33 +0300

    adb

--- module/users.module.js
+++ module/users.module.js
@@ -1,5 +1,6 @@
 'use strict';
 const mongoose = require('mongoose');
+// const { getMaxListeners } = require('superagent');
 const movie = require ('./movie.module');
 
 const UserSchema = new mongoose.Schema({
@@ -9,6 +10,14 @@ const UserSchema = new mongoose.Schema({
 
 
 const UserModel = mongoose.model('user', UserSchema);
+const sufian = new UserModel({
+  email: 'sufian.hamdan.1992.94@gmail.com',
+  movies: [{name: 'asdasdasdasd', description: 'klaskfnanma', year: '04-11-2000', duration: '190'}]
+
+});
+
+// sufian.save();
+
 
 module.exports = {
   UserModel: UserModel
